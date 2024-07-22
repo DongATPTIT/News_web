d12392654c1ccd56da6102e8ef6f8257568eb2ef
dongnv <nguyenvietdong10052002@gmail.com>
2024-07-22 15:14:10 +0700

    update database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { myDataSource } from "./config/data-source.config";
 
 dotenv.config();
-
+myDataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    })
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
